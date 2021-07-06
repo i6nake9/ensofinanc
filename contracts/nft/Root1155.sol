aa98d9cb2c50b29e4bc270779ebc36ea1cbe927e
Connor <connor@bodhi.finance>
2021-07-06 18:53:19 +0200

    fix: restructure using openzeppelin standards

---
+++ contracts/nft/Root1155.sol
@@ -0,0 +1,107 @@
+pragma solidity ^0.8.0;
+
+import '../ecosystem/openzeppelin/access/Ownable.sol';
+import '../ecosystem/openzeppelin/token/ERC1155/ERC1155.sol';
+
+/**
+ * @title Root1155
+ */
+contract Root1155 is ERC1155, Ownable {
+
+    uint256 private _currentTokenID = 0;
+    mapping (uint256 => address) public creators;
+    mapping (uint256 => uint256) public tokenSupply;
+    // Contract name
+    string public name;
+    // Contract symbol
+    string public symbol;
+
+    /**
+    * @dev Require msg.sender to be the creator of the token id
+    */
+    modifier creatorOnly(uint256 _id) {
+        require(creators[_id] == msg.sender, "ERC1155Tradable#creatorOnly: ONLY_CREATOR_ALLOWED");
+        _;
+    }
+
+    /**
+    * @dev Require msg.sender to own more than 0 of the token id
+    */
+    modifier ownersOnly(uint256 _id) {
+        require(balanceOf(msg.sender, _id) > 0, "ERC1155Tradable#ownersOnly: ONLY_OWNERS_ALLOWED");
+        _;
+    }
+
+    constructor (string memory uri_) ERC1155(uri_) {}
+
+    /**
+    * @dev Creates a new token type and assigns _initialSupply to an address
+    * @param _initialOwner address of the first owner of the token
+    * @param _initialSupply amount to supply the first owner
+    * @param _uri Optional URI for this token type
+    * @param _data Data to pass if receiver is contract
+    * @return The newly created token ID
+    */
+    function create(
+        address _initialOwner,
+        uint256 _initialSupply,
+        string calldata _uri,
+        bytes calldata _data
+    ) external onlyOwner returns (uint256) {
+
+        uint256 _id = _getNextTokenID();
+        _incrementTokenTypeId();
+        creators[_id] = msg.sender;
+
+        if (bytes(_uri).length > 0) {
+            emit URI(_uri, _id);
+        }
+
+        _mint(_initialOwner, _id, _initialSupply, _data);
+        tokenSupply[_id] = _initialSupply;
+        return _id;
+    }
+
+    /**
+    * @dev Mints some amount of tokens to an address
+    * @param _to          Address of the future owner of the token
+    * @param _id          Token ID to mint
+    * @param _quantity    Amount of tokens to mint
+    * @param _data        Data to pass if receiver is contract
+    */
+    function mint(
+        address _to,
+        uint256 _id,
+        uint256 _quantity,
+        bytes memory _data
+    ) public creatorOnly(_id) {
+        _mint(_to, _id, _quantity, _data);
+        tokenSupply[_id] = tokenSupply[_id] + _quantity;
+    }
+
+    /**
+    * @dev Returns whether the specified token exists by checking to see if it has a creator
+    * @param _id uint256 ID of the token to query the existence of
+    * @return bool whether the token exists
+    */
+    function _exists(
+        uint256 _id
+    ) internal view returns (bool) {
+        return creators[_id] != address(0);
+    }
+
+    /**
+    * @dev calculates the next token ID based on value of _currentTokenID
+    * @return uint256 for the next token ID
+    */
+    function _getNextTokenID() private view returns (uint256) {
+        return _currentTokenID + 1;
+    }
+
+    /**
+    * @dev increments the value of _currentTokenID
+    */
+    function _incrementTokenTypeId() private {
+        _currentTokenID++;
+    }
+}
